e924af5e1402cfb3547f48c978e212ae09d14918
yuma-higa <126500372+yuma-higa@users.noreply.github.com>
2024-04-19 15:17:44 +0900

    first commit

---
+++ app/(root)/communities/page.tsx
@@ -0,0 +1,11 @@
+const Page = async () => {
+    return (
+        <section>
+            <h1 className = "head-text mb-10">
+                communities
+            </h1>
+        </section>
+    )
+}
+
+export default Page;
